6339796e704cb820139939e414e181b427fb4d24
Piotr Czajka <piotr.czajka@ttas.pl>
2021-07-06 20:59:11 +0200

    check how headers work

---
+++ src/lib.cpp
@@ -0,0 +1,6 @@
+#include "lib.h"
+#include <iostream>
+
+void printHelloWorld() {
+    std::cout << "Hello world\n";
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
+#include "lib.h"
 
 int main() {
-    std::cout << "Hello world\n";
+    printHelloWorld();
 }
